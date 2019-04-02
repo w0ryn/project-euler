c4ec760fd3b3939db488278a0c72c11c9d2dd4a2
Wryn Wagner <wagner.wryn@gmail.com>
2019-04-02 16:06:43 -0600

    (no.3) : CLISP update :: refactored code

--- 03-largest-prime-factor/clisp/largest-prime.lisp
+++ 03-largest-prime-factor/clisp/largest-prime.lisp
@@ -1,41 +1,28 @@
-; recursive! stack overflow when processing large numbers
-(defun recFactorize (num &optional (i 2) (maxFactor (sqrt num)))
-  (if (and (> maxFactor i) (> num 2))
-      (if (= (mod num i) 0); if 'i' is a factor, add it to the list; otherwise, try the next potential prime
-          (cons i (factorize (/ num i) i maxFactor))
-          (factorize num (if (= i 2) 3 (+ i 2)) maxFactor)); note: 'i' is ugly here to account for the first iteration when 'i=2'
-      (list num))); any non-1 remainder is also a prime factor
-
-; not recursive, but I'm not sure if this is necessarily in the spirit of LISP
+; returns a list containing the prime factors of a number
 (defun factorize (num)
   (setq i 3 maxFactor (sqrt num))
   ; factor out all the twos
   (setq factors (loop while (= (mod num 2) 0)
-	      do (setq num (/ num 2))
-	      collect 2))
-
+		      do (setq num (/ num 2))
+		      collect 2))
   ; after all twos are factored out, every possible remaining prime is odd
-  ; search for all primes less than the square root of the original
-  (loop while (and (<= i maxFactor) (> num 2))
-	do (if (= (mod num i) 0)
-	     (progn
-	       (setq factors (append factors (list i)))
-	       (setq num (/ num i)))
-	     (setq i (+ i 2))))
-
-  ; any non-1 remainder is also a prime factor
+  (loop while (and (<= i maxFactor) (> num 2)); search every odd divisor less than the square root of the original number
+	do (if (= (mod num i) 0); check to see if (i) is a new factor
+	     (setq
+	       factors (append factors (list i)); add the new factor to the list
+	       num (/ num i)))
+	     (setq i (+ i 2))); check the next odd divisor if the current (i) is not a factor
+  ; any non-1 remainder must also be a prime factor
   (if (> num 1)
     (setq factors (append factors (list num))))
-
-  ; return the list of prime factors
-  factors)
-  
+  factors); return the list of prime factors
 
 (defun main ()
-  (setq myNumber 6008514722372248)
-  (time (setq primeFactors (factorize myNumber)))
-  (format t "~%~%List of all prime factors for the number ~D: ~%~D~%"
-	  myNumber primeFactors)
-  (format t "~%Largest prime factor: ~%~D~%" (last primeFactors)))
+  (setq myNumber 600851475143); number from problem definition
+  (time (setq primeFactors (factorize myNumber))); time the calculation
+  (format t "
+	  ~%List of all prime factors for the number ~D: ~%~D~%
+	  ~%Largest prime factor: ~%~D~%"
+	  myNumber primeFactors (last primeFactors))); user-friendly output :)
 
 (main)
