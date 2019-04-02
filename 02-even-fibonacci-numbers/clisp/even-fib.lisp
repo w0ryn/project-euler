1a210684d5f41e699a23ccf28bb987f04ffddff1
Wryn Wagner <wagner.wryn@gmail.com>
2019-04-02 15:53:18 -0600

    (no.2) : CLISP update :: refactored code

--- 02-even-fibonacci-numbers/clisp/even-fib.lisp
+++ 02-even-fibonacci-numbers/clisp/even-fib.lisp
@@ -1,27 +1,28 @@
-; for my own reference, calculating the first n fibonacci terms
-(defun fibonacci (n &optional (a 0) (b 1))
-  (if (zerop n)
-    nil
-    (cons a (fibonacci (1- n) b (+ a b)))))
+; calculates fibonacci terms until a single term exceeds an indicated (maxTerm)
+(defun fibUnderX (maxTerm &optional (n (list 0)) (a 1) (b 1))
+  (if (< (car (last n)) maxTerm)
+    (fibUnderX maxTerm (append n (list a)) b (+ a b)); continue if under (maxTerm)
+    (butlast n))); return the list of all terms less than (maxTerm)
 
-(defun fibUnder4mil (&optional (n (list 0)) (a 1) (b 1))
-  (if (< (car (last n)) 4000000)
-    (fibUnder4mil (append n (list a)) b (+ a b)); if we are under 4,000,000 continue to the next fibonacci term
-    (butlast n))); if we have gone over 4,000,000 return the list excluding the final element
-
+; given a list of numbers, returns a list containing all even terms
 (defun getEvens (numbers)
   (loop for x in numbers
 	if (= (mod x 2) 0) collect x))
 
+; returns the sum of a list
 (defun sumList (numbers)
   (if (null numbers) 0 
     (+ (car numbers) (sumList (cdr numbers)))))
 
 (defun main ()
-  (setq fib<4mil (fibUnder4mil))
-  (setq evenfib<4mil (getEvens fib<4mil))
-  (format t "~%List of all fibonacci terms less than 4,000,000: ~%~D~%" fib<4mil)
-  (format t "~%List of all even fibonacci terms less than 4,000,000: ~%~D~%" evenfib<4mil)
-  (format t "~%Sum of all even fibonacci terms less than 4,000,000: ~%~D~%" (sumList evenfib<4mil)))
+  (setq
+    fib<4mil (fibUnderX 4000000); calculate all fibonacci terms less than 4,000,000
+    evenFib<4mil (getEvens fib<4mil); determine all the even terms
+    sumEvenFib<4mil (sumList evenFib<4mil)); sum together the even terms
+  (format t "
+	  ~%List of all fibonacci terms less than 4,000,000: ~%~D
+	  ~%List of all even fibonacci terms less than 4,000,000: ~%~D
+	  ~%Sum of all even fibonacci terms less than 4,000,000: ~%~D"
+	  fib<4mil evenfib<4mil sumEvenFib<4mil)); user-friendly output
 
 (main)
