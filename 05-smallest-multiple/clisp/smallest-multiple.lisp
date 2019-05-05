6fb2313418a632ac39d764840502ae7d65ddeebb
Wryn Wagner <wagner.wryn@gmail.com>
2019-05-04 21:46:31 -0600

    (no.5) : CLISP update :: refactored code

--- 05-smallest-multiple/clisp/smallest-multiple.lisp
+++ 05-smallest-multiple/clisp/smallest-multiple.lisp
@@ -1,13 +1,3 @@
-; return whether t if (x) is evenly divisible by all integers 1 through (n)
-(defun isDivisibleBy1toN (x n)
-  (cond ((= n 1) t); base case: 1 evenly divides all x
-	((= (mod x n) 0); if x is evenly divisible by n
-	  (isDivisibleBy1toN x (1- n))))); check n-1
-; return the smallest integer evenly divisible by integers 1 through (n) (brute force method)
-(defun smallestMultiple (n)
-  (do ((x 1 (1+ x)))
-      ((isDivisibleBy1toN x n) x)))
-
 ; most practical solution: use the built-in (lcm) function (which accepts multiple integer arguments)
 (defun builtInApproach (n)
        (apply #'lcm (loop for x from 1 to n collecting x)))
@@ -15,21 +5,16 @@
 ; for the purpose of the exercise, we'll build out the lcm function:
 
 ; implementation of the Euclidean greatest common divisor algorithm
-;   identity :: gcd(x, 0) = x
-; recurrence :: gcd(x, y) = gcd(y, x%y)
 (defun gcd* (x y)
   (do ()
-      ((zerop y) (abs x))
-      (shiftf x y (mod x y))))
+      ((zerop y) (abs x))      ; identity :: gcd(x, 0) = x
+      (shiftf x y (mod x y)))) ; recurrence :: gcd(x, y) = gcd(y, x%y)
 
 ; implementation of the least common multiple equation (based on GCD)
-;      equation :: lcm(x, y) = | x * y | / gcd(x,y)
-; associativity :: lcm(x, y, z) = lcm( lcm(x, y) z)
-;      identity :: lcm(x, 1) = x
 (defun lcm* (&rest numbers)
-  (reduce (lambda (x y)
-	    (/ (abs (* x y)) (gcd* x y)))
-	  numbers :initial-value 1))
+  (reduce (lambda (x y)                   ; associativity :: lcm(x, y, z) = lcm( lcm(x, y) z)
+	    (/ (abs (* x y)) (gcd* x y))) ; equation :: lcm(x, y) = |x*y| / gcd(x,y)
+	  numbers :initial-value 1))      ; identity :: lcm(x, 1) = x
 
 ; then we can use the built-out lcm in the same way as the built-in
 (defun lcmFor1toN (n)
